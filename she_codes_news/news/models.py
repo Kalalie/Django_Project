02535cb36c4cbd919fd18b0cb461e8e6746c1d18
Kalalie <67173676+Kalalie@users.noreply.github.com>
2020-08-05 19:42:42 +0800

    Login and logout button

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
     image_url = models.URLField(max_length=200, blank=True, null=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    
     def __str__(self):
         return self.title + str(self.pub_date)
--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create your tests here. this is where I would put my own fields
+class CustomUser(AbstractUser):
+
+    def _str_(self):
+        return self.username
+
+
+
